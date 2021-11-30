38017c617c83011a9ad05fcaee6c1a068f9b07aa
nestorsokil <nestor0603@gmail.com>
2021-11-30 15:16:34 +0200

    Updated example

--- examples/main.tf
+++ examples/main.tf
@@ -1,54 +1,103 @@
 provider "vgs" {}
 
-resource "vgs_route" "my_route" {
+resource "vgs_route" "inbound_route" {
   environment = "sandbox"
   vault = "tntbcduzut5"
   inline_config = <<EOF
 id: 04b2e1b7-fb60-472f-a79f-af7e2353f122
 type: rule_chain
 attributes:
-  created_at: '2021-11-26T18:10:08'
+  tags:
+    name: my-awesome-inbound-route
+    source: RouteContainer
   destination_override_endpoint: 'https://echo.apps.verygood.systems'
-  entries:
-    - classifiers: {}
-      config:
-        condition: AND
-        rules:
-          - expression:
-              field: PathInfo
-              operator: matches
-              type: string
-              values:
-                - /post
-          - expression:
-              field: ContentType
-              operator: equals
-              type: string
-              values:
-                - application/json
-            rules: null
-      id: 39f2f5db-06a0-461d-9387-dd9a7ab19035
-      id_selector: null
-      operation: REDACT
-      operations: null
-      phase: REQUEST
-      public_token_generator: UUID
-      targets:
-        - body
-      token_manager: PERSISTENT
-      transformer: JSON_PATH
-      transformer_config:
-        - $.account_number
-      transformer_config_map: null
   host_endpoint: (.*)\.verygoodproxy\.io
   id: 04b2e1b7-fb60-472f-a79f-af7e2353f122
   ordinal: null
   port: 80
   protocol: http
   source_endpoint: '*'
+  entries:
+  - classifiers: {}
+    config:
+      condition: AND
+      rules:
+        - expression:
+            field: PathInfo
+            operator: matches
+            type: string
+            values:
+              - /post
+        - expression:
+            field: ContentType
+            operator: equals
+            type: string
+            values:
+              - application/json
+          rules: null
+    id: 39f2f5db-06a0-461d-9387-dd9a7ab19035
+    id_selector: null
+    operation: REDACT
+    operations: null
+    phase: REQUEST
+    public_token_generator: UUID
+    targets:
+      - body
+    token_manager: PERSISTENT
+    transformer: JSON_PATH
+    transformer_config:
+      - $.account_number
+    transformer_config_map: null
+EOF
+}
+
+resource "vgs_route" "outbound_route" {
+  environment = "sandbox"
+  vault = "tntbcduzut5"
+  inline_config = <<EOF
+id: 37df6406-817f-4817-a72a-c675c50fb8ac
+type: rule_chain
+attributes:
   tags:
-    name: echo.apps.verygood.systems-beige-crescent
+    name: my-awesome-outbound-route
     source: RouteContainer
-  updated_at: '2021-11-26T18:10:08'
+  destination_override_endpoint: '*'
+  host_endpoint: echo\.apps\.verygood\.systems
+  id: 37df6406-817f-4817-a72a-c675c50fb8ac
+  ordinal: null
+  port: 80
+  protocol: http
+  source_endpoint: '*'
+  entries:
+  - classifiers: {}
+    config:
+      condition: AND
+      rules:
+        - expression:
+            field: PathInfo
+            operator: matches
+            type: string
+            values:
+              - /post
+        - expression:
+            field: ContentType
+            operator: equals
+            type: string
+            values:
+              - application/json
+    id: ea83a6fd-9d04-42b8-8ca0-d51b98380b2e
+    id_selector: null
+    operation: ENRICH
+    operations: null
+    phase: REQUEST
+    public_token_generator: UUID
+    targets:
+      - body
+    token_manager: PERSISTENT
+    transformer: JSON_PATH
+    transformer_config:
+      - $.account_number
+    transformer_config_map: null
+    type: null
 EOF
 }
